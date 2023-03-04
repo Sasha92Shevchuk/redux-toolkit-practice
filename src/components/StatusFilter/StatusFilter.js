054e3d0e58136f145f48676b8e12a7a7fcad481f
Sasha92Shevchuk <108683139+Sasha92Shevchuk@users.noreply.github.com>
2023-03-04 17:53:20 +0200

    before redux toolkit

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,12 +1,42 @@
 import { Button } from 'components/Button/Button';
 import css from './StatusFilter.module.css';
+// Імпортуємо хук
+import { useSelector, useDispatch } from 'react-redux';
+// Імпортуємо об'єкт значень фільтра
+import { statusFilters } from '../../redux/constants';
+import { getStatusFilter } from 'redux/selectors';
+// Імпортуємо генератор екшену
+import { setStatusFilter } from '../../redux/actions';
 
 export const StatusFilter = () => {
+  // Отримуємо значення фільтра із стану Redux
+  // const filter = useSelector(state => state.filters.status); - замінюємо на функціюімпорт
+  const filter = useSelector(getStatusFilter);
+
+  // Отримуємо посилання на функцію відправки екшенів
+  const dispatch = useDispatch();
+  const handleFilterChange = filter => dispatch(setStatusFilter(filter));
+
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button
+        selected={filter === statusFilters.all}
+        onClick={() => handleFilterChange(statusFilters.all)}
+      >
+        All
+      </Button>
+      <Button
+        selected={filter === statusFilters.active}
+        onClick={() => handleFilterChange(statusFilters.active)}
+      >
+        Active
+      </Button>
+      <Button
+        selected={filter === statusFilters.completed}
+        onClick={() => handleFilterChange(statusFilters.completed)}
+      >
+        Completed
+      </Button>
     </div>
   );
 };
---
+++ src/redux/reducer.js
@@ -0,0 +1,119 @@
+// Імпортуємо функцію композиції редюсерів
+import { combineReducers } from 'redux';
+import { statusFilters } from './constants';
+//  щоб не робити занадто великим кореневий редюсер ділимо його на частини, кожен з яких буде відповідати за свою частину стану і тоді код буде чистішим
+// const initialState = {
+//   tasks: [
+//     { id: 0, text: 'Learn HTML and CSS', completed: true },
+//     { id: 1, text: 'Get good at JavaScript', completed: true },
+//     { id: 2, text: 'Master React', completed: false },
+//     { id: 3, text: 'Discover Redux', completed: false },
+//     { id: 4, text: 'Build amazing apps', completed: false },
+//   ],
+//   filters: {
+//     status: statusFilters.all,
+//   },
+// };
+
+// export const rootReducer = (state = initialState, action) => {
+//   // Редюсер розрізняє екшени за значенням властивості type
+//   switch (action.type) {
+//     // Залежно від типу екшену виконуватиметься різна логіка
+//     case 'tasks/addTask': {
+//       // Потрібно повернути новий об'єкт стану
+//       return {
+//         // в якому є всі дані існуючого стану
+//         ...state,
+//         // та новий масив задач
+//         tasks: [
+//           // в якому є всі існуючі завдання
+//           ...state.tasks,
+//           // та об'єкт нового завдання
+//           action.payload,
+//         ],
+//       };
+//     }
+//     //   для видалення фільтруємо на співпадіння Id
+//     case 'tasks/deleteTask':
+//       return {
+//         ...state,
+//         tasks: state.tasks.filter(task => task.id !== action.payload),
+//       };
+//     //Перевіряємо, чи відповідає тип відправленого екшену рядку "tasks/toggleCompleted" та повертаємо новий об'єкт стану.
+//     case 'tasks/toggleCompleted':
+//       return {
+//         ...state,
+//         tasks: state.tasks.map(task => {
+//           if (task.id !== action.payload) {
+//             return task;
+//           }
+//           return {
+//             ...task,
+//             completed: !task.completed,
+//           };
+//         }),
+//       };
+//     //перевіряємо чи відповідає тип відправленого екшену рядку "filters/setStatusFilter" та повертаємо новий об'єкт стану.
+//     case 'filters/setStatusFilter':
+//       return {
+//         ...state,
+//         filters: {
+//           ...state.filters,
+//           status: action.payload,
+//         },
+//       };
+//     default:
+//       // Кожен редюсер отримує всі екшени, відправлені в стор.
+//       // Якщо редюсер не повинен обробляти якийсь тип екшену,
+//       // необхідно повернути наявний стан без змін.
+//       return state;
+//   }
+// };
+
+//  оновлений і правильний код
+const tasksInitialState = [
+  { id: 0, text: 'Learn HTML and CSS', completed: true },
+  { id: 1, text: 'Get good at JavaScript', completed: true },
+  { id: 2, text: 'Master React', completed: false },
+  { id: 3, text: 'Discover Redux', completed: false },
+  { id: 4, text: 'Build amazing apps', completed: false },
+];
+
+const tasksReducer = (state = tasksInitialState, action) => {
+  switch (action.type) {
+    case 'tasks/addTask':
+      return [...state, action.payload];
+    case 'tasks/deleteTask':
+      return state.filter(task => task.id !== action.payload);
+    case 'tasks/toggleCompleted':
+      return state.map(task => {
+        if (task.id !== action.payload) {
+          return task;
+        }
+        return { ...task, completed: !task.completed };
+      });
+    default:
+      return state;
+  }
+};
+
+const filtersInitialState = {
+  status: statusFilters.all,
+};
+
+const filtersReducer = (state = filtersInitialState, action) => {
+  switch (action.type) {
+    case 'filters/setStatusFilter':
+      return {
+        ...state,
+        status: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const rootReducer = combineReducers({
+  tasks: tasksReducer,
+  filters: filtersReducer,
+});
